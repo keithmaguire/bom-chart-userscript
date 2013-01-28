948937a36245d551620d7aecb003efaa4fb3a926
Keith Maguire <keithmaguire@fastmail.com.au>
2013-01-28 18:46:27 +1030

    Added grid lines, forecast and page (or tab) title

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A greasemonkey userscript to add charts to the (Australian) Bureau of Meteorolog
 This is a greasemonkey script using [d3.js](http://www.d3js.org) to add a temperature and humidity graph
 to the top of the Recent Observations pages,such as this one for [Adelaide](http://www.bom.gov.au/products/IDS60901/IDS60901.94675.shtml)
 
+It's mostly because I really wanted to have this, but it's been also a useful exercise for starting d3
+
 From
 
 
@@ -17,17 +19,15 @@ There are two here - the first one uses the data as presented in the tables on t
 
 I'm going to keep the table one here mainly as a reference for myself and work on the json based one, as I think that can probably be gotten to do more interesting things.
 
-It's mostly because I really wanted to have this, but it's been also a useful exercise for starting d3
+
 
 There are a number of updates I'd like to do - 
 
- 1. make it look a bit nicer - add a bit of shading over the portions that are the night and maybe faint lines in the background to 
-help with seeing the values? 
- 2. at the moment it uses svg:title to do simple pop-ups on mouseover for specific values
+ 
+ 1. at the moment it uses svg:title to do simple pop-ups on mouseover for specific values
 so I'd like to update that to use jquery and tipsy instead
- 3. I also want to just tidy it up somewhat - I think I've needlessly reproduced things. This includes separating out the CSS, probably using Greasemonkey to add the different classes to the pages HEAD
- 4. and then after that try and work out if it's possible (and easy) to import details like the forecasted max and min from other pages. I think that'd be too difficult to do as the forecast URLs have a different code in them for each place than the observations but I'll give it a go...I've gotten the potential forecast page URLs out in the code, but they're not all there so not sure what to do with that
- 5. I'd like it to default to showing a day but be easy to zoom out to show the 72hour view
+ 2. I also want to just tidy it up somewhat - I think I've not properly wrapped my head around how things should be done with d3 and have needlessly duplicated things that should have been abstracted. This includes separating out the CSS, probably using Greasemonkey to add the different classes to the pages HEAD
+
 
 
 But it works perfectly fine at the moment!
--- bom-chart-json.user.js
+++ bom-chart-json.user.js
@@ -2,160 +2,184 @@
 // @name        bom-chart-json
 // @namespace   keithmaguire.net
 // @require     http://d3js.org/d3.v3.min.js
+// @require     http://code.jquery.com/jquery-1.9.0.min.js
 // @description put charts onto BOM using the observations json feed
 // @include     http://www.bom.gov.au/products/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
+$('head').append("<link href='http://www.bom.gov.au/css/weather/gfe.css' type='text/css' rel='stylesheet'>");
 
-// current is the observations url, for example: http://www.bom.gov.au/products/IDS60901/IDS60901.94675.shtml
-var current = window.location + "";
-var url_parts = current.split("products"); 
-var base_bom_url = url_parts[0]; // "http://www.bom.gov.au/"
+///////Create a div to put the chart into
+$('<div id="obs_chart"></div>').insertBefore('.p-id');
 
-var product_id_url = url_parts[1];
-var htmlurl = base_bom_url + "fwo" + product_id_url;
-var jsonurl = htmlurl.replace("shtml","json")
 
+//getting the URL for the json file attached to the observations
+
+var current = window.location + "",
+    url_parts = current.split("products"),
+    base_bom_url = url_parts[0], 
+    product_id_url = url_parts[1], 
+    htmlurl = base_bom_url + "fwo" + product_id_url,
+    jsonurl = htmlurl.replace("shtml", "json");
 console.log(jsonurl);
 
-document.body.innerHTML= document.body.innerHTML.replace(/<div class="p-id">.*>/,'<div class="p-id"><p></p></div>');
+d3.json(jsonurl, function (json) {
 
 
-d3.json(jsonurl, function(json){
+//forecast url
+    var state_uppercase = json.observations.header[0].state_time_zone,
+        state = state_uppercase.toLowerCase(),
+        town_titlecase = json.observations.header[0].name,
+        town = town_titlecase.split(" ")[0].toLowerCase(),
+        forecast_url = base_bom_url + state + "/forecasts/" + town + ".shtml";
+        
+    console.log(forecast_url);
+    
+    
+    var obsData = json.observations.data;
 
-//forecast url - if it exists, to be investigated at another time!
-var state_uppercase = json.observations.header[0].state_time_zone;
-var state = state_uppercase.toLowerCase();
-console.log(state);
-var town_titlecase = json.observations.header[0].name;
-var town = town_titlecase.replace(" ","").toLowerCase();
-console.log(town);
-var forecast_url = base_bom_url + state + "/forecasts/" + town + ".shtml"
-console.log(forecast_url)
+//so chart reads from left to right
+    obsData.reverse();
 
-var obsData = json.observations.data;
+    var margin = 30,
+        chartHeight = 400,
+        chartWidth = 793;
 
-obsData.reverse();
-
-var margin = 45
-var chartHeight = 400 
-var chartWidth = 793
-
-//time entries are in the format 20130119190000
-var parseDate = d3.time.format("%Y%m%d%H%M%S").parse;
-
-obsData.forEach(function(d) {
-    d.local_date_time_full = parseDate(d.local_date_time_full);
+//in the data change local_date_time_full from a string to a date/time value
+    var parseDate = d3.time.format("%Y%m%d%H%M%S").parse;
+    obsData.forEach(function (d) {
+        d.local_date_time_full = parseDate(d.local_date_time_full);
     });
 
-console.log(obsData[0]);
+    var lengthofdata = obsData.length;
 
-
-
-// mapping the object generated from the table, attaching the appropriate keys
-// to each entry as well as converting the numbers to numbers rather than strings
-
-var lengthofdata = obsData.length;
-
-    
 //////////////////////////////
 /////////// Drawing the Chart
 //////////////////////////////
 
-//var x = d3.scale.linear().domain([0, 160]).range([margin + 0, chartWidth - margin])
-var x = d3.time.scale().domain(d3.extent(obsData, function(d) {return d.local_date_time_full}))
-                       .range([margin + 0, chartWidth - margin])
-var y1 = d3.scale.linear().domain([0, 50]).range([chartHeight - margin, 0 + margin])
-var y2 = d3.scale.linear().domain([0, 100]).range([chartHeight - margin, 0 + margin])
-//temperature hopefully never goes above 50 degrees
+///////////////The scales
+    var x = d3.time.scale().domain(d3.extent(obsData, function(d) {return d.local_date_time_full}))
+                       .range([margin + 0, chartWidth - margin]);
+//temperature - hopefully never goes above 50 degrees
+    var y1 = d3.scale.linear().domain([0, 50]).range([chartHeight - margin, 0 + margin]);
 //humidity goes from 0 to 100 percent
+    var y2 = d3.scale.linear().domain([0, 100]).range([chartHeight - margin, 0 + margin]);
 
-//The SVG Container
-var svgContainer = d3.select(".p-id").append("svg")
-                                    .attr("width", chartWidth)
-                                    .attr("height", chartHeight);
+//The SVG Container to hold the chart
+    var svgContainer = d3.select("#obs_chart").append("svg")
+                                     .attr("width", chartWidth)
+                                     .attr("height", chartHeight);
+    var g = svgContainer.append("svg:g");
 
-var g = svgContainer.append("svg:g");
+///////////////The grid lines
+// Draw X-axis grid lines - thick ones for day
+    svgContainer.selectAll("line.x")
+                .data(x.ticks(3))
+                .enter().append("line")
+                .attr("x1", x)
+                .attr("x2", x)
+                .attr("y1", 0 + margin)
+                .attr("y2", chartHeight - margin)
+                .style("stroke-width", 2)
+                .attr("stroke","#ccc")
+                .attr("opacity", 0.5);
 
-//The axes
+// Draw X-axis grid lines - thinner ones for every six hours
+    svgContainer.selectAll("line.x")
+                .data(x.ticks(12))
+                .enter().append("line")
+                .attr("x1", x)
+                .attr("x2", x)
+                .attr("y1", 0 + margin)
+                .attr("y2", chartHeight - margin)
+                .style("stroke-width", 2)
+                .attr("stroke","#ccc")
+                .attr("opacity", 0.3);
+
+// Draw Y-axis grid lines
+    svgContainer.selectAll("line.y")
+                .data(y1.ticks(5))
+                .enter().append("line")
+                .attr("x1", margin)
+                .attr("x2", chartWidth - margin)
+                .attr("y1", y1)
+                .attr("y2", y1)
+                .style("stroke", "#ccc");
+
+//////////////////The axes
 
 //x axis
-var xaxis = d3.svg.axis()
-                   .scale(x)
-                   .orient("bottom");
-                   
-svgContainer.append("g")
-            .attr("fill","none")
-            .attr("stroke","black")
-            .attr("ticks",48)
-            .attr("stroke-width","0.5")
-            .attr("transform", "translate(0," + (chartHeight - margin ) + ")")
+    var xaxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("bottom");
+
+    svgContainer.append("g")
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("ticks", 48)
+            .attr("stroke-width", "0.5")
+            .attr("transform", "translate(0," + (chartHeight - margin) + ")")
             .call(xaxis);          
 
 //air_temp axis
-var y1axis = d3.svg.axis()
+    var y1axis = d3.svg.axis()
                    .scale(y1)
                    .ticks(10)
                    .orient("left");
-                 
-svgContainer.append("g")
-            .attr("fill","none")
-            .attr("stroke","red")
-            .attr("stroke-width","0.5")
-            .attr("transform","translate(" + (2 + margin) + ",0)")
+
+    svgContainer.append("g")
+            .attr("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-width", "0.5")
+            .attr("transform", "translate(" + ( margin) + ",0)")
             .call(y1axis)
           .append("text")
             .attr("y", 6)
-            .attr("transform","translate(-10, 0)")
+            .attr("transform", "translate(-10, 0)")
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text("ºC");
 
 //humidity axis
-var y2axis = d3.svg.axis()
+    var y2axis = d3.svg.axis()
                    .scale(y2)
                    .orient("right");
 
-svgContainer.append("g")
-            .attr("fill","none")
-            .attr("stroke","blue")
-            .attr("stroke-width","0.5")
-            .attr("transform","translate(" + margin + ",0)")
+    svgContainer.append("g")
+            .attr("fill", "none")
+            .attr("stroke", "blue")
+            .attr("stroke-width", "0.5")
+            .attr("transform", "translate(" + margin + ",0)")
             .call(y2axis)
           .append("text")
             .attr("y", 6)
-            .attr("transform","translate(20, 0)")
+            .attr("transform", "translate(20, 0)")
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text("%");
 
-//put shaded rectangles over what's pretty much always night in Aus - 9pm to 4am
-//
-//
-//
-//
+
+
 //the lines
-var last_temp_y = y1(obsData[lengthofdata - 1].air_temp);
-var last_humidity_y = y2(obsData[lengthofdata - 1].rel_hum);
 
 //prepares the humidity line
-var lineFunction = d3.svg.line()
+    var lineFunction = d3.svg.line()
                          .x(function(d) { return x((d.local_date_time_full)); })
                          .y(function(d) { return y2(d.rel_hum); })
                          .interpolate("linear");
 
 //draws the humidity line
-var lineGraph = g.append("path")
+    var lineGraph = g.append("path")
                  .attr("d", lineFunction(obsData))
                  .attr("stroke", "lightskyblue")
-                 .attr("opacity","0.8")
+                 .attr("opacity", "0.8")
                  .attr("stroke-width", 2)
                  .attr("fill", "none");
                            
 //draws circles for mouseover
-g.selectAll("humidcircle")
+    g.selectAll("humidcircle")
           .data(obsData)
           .enter()
           .append("svg:circle")
@@ -164,43 +188,43 @@ g.selectAll("humidcircle")
           .attr("r", 8)
           .attr("opacity", 0)
         .append("svg:title")
-          .text(function(d){
+          .text(function (d) {
             return d.rel_hum
             + "% at " 
             + d.local_date_time
            });
-          
+
 //prepares the temperature line
-var lineFunction = d3.svg.line()
+    var lineFunction = d3.svg.line()
                          .x(function(d) { return x((d.local_date_time_full)); })
                          .y(function(d) { return y1(d.air_temp); })
                          .interpolate("linear");
 
 //draws the temperature line
-var lineGraph = g.append("path")
+    var lineGraph = g.append("path")
                  .attr("d", lineFunction(obsData))
                  .attr("stroke", "red")
-                 .attr("opacity","0.8")
+                 .attr("opacity", "0.8")
                  .attr("stroke-width", 2)
                  .attr("fill", "none");
                  
 //prepares the apparent temperature line
-var lineFunction = d3.svg.line()
+    var lineFunction = d3.svg.line()
                          .x(function(d) { return x((d.local_date_time_full)); })
                          .y(function(d) { return y1(d.apparent_t); })
                          .interpolate("linear");
 
 //draws the apparent temperature line
-var lineGraph = g.append("path")
+    var lineGraph = g.append("path")
                  .attr("d", lineFunction(obsData))
                  .attr("stroke", "red")
-                 .attr("opacity","0.25")
+                 .attr("opacity", "0.25")
                  .attr("stroke-width", 2)
-                 .attr("stroke-dasharray","5,3")
+                 .attr("stroke-dasharray", "5,3")
                  .attr("fill", "none");
                             
 //draws circles for mouseover
-g.selectAll("tempcircle")
+    g.selectAll("tempcircle")
           .data(obsData)
           .enter()
           .append("svg:circle")
@@ -209,57 +233,60 @@ g.selectAll("tempcircle")
           .attr("r", 8)
           .attr("opacity", 0)
         .append("svg:title")
-          .text(function(d){
+          .text(function (d) {
           return d.air_temp
             + "° at " 
             + d.local_date_time
           });
 
-//midpoint between the final point of temperature and humidity                
-var midpoint = 0.5*(last_temp_y + last_humidity_y);
+/////////////////Annotations and titles
 
+//The final values in each line and the midpoint between them
+    var last_temp_y = y1(obsData[lengthofdata - 1].air_temp),
+        last_humidity_y = y2(obsData[lengthofdata - 1].rel_hum),
+        midpoint = 0.5*(last_temp_y + last_humidity_y);
 
 //add the time of the last observation to the page's (h1) title
-var last_time = obsData[lengthofdata - 1].local_date_time.split("/")[1];
+    var last_time = obsData[lengthofdata - 1].local_date_time.split("/")[1];
 
-d3.select("h1").append("text")
-            .text(", as of " 
-            + last_time 
-            + ": "
-            + obsData[lengthofdata - 1].air_temp + "°C" 
-            + " and " 
-            + obsData[lengthofdata - 1].rel_hum + "%"
-            + " humidity");
+    d3.select("h1").append("text")
+                   .text(" ("
+                   + last_time 
+                   + "): "
+                   + obsData[lengthofdata - 1].air_temp + "°C" 
+                   + " and " 
+                   + obsData[lengthofdata - 1].rel_hum + "%"
+                   + " humidity");
 
-//Change page title
-document.title = town_titlecase +
-            ", " 
-            + last_time +": "
-            + obsData[lengthofdata - 1].air_temp + "°C," 
-            + obsData[lengthofdata - 1].rel_hum + "%";
+//change the page title so you can see it in the tab title when on another page
+    document.title = obsData[lengthofdata - 1].air_temp + "°C, " 
+                     + obsData[lengthofdata - 1].rel_hum + "%, "
+                     + last_time + " ("                
+                     + town_titlecase +")";
 
+//////////////////add the final value to the end of each line
             
-var topvalue,bottomvalue,topcolour,bottomcolour;
-//if the values are too close together to be displayed neatly just display them
-//above and below the date
-if ( midpoint - last_temp_y > 10)
-  {
+    var topvalue, bottomvalue, topcolour, bottomcolour;
+    
+//if the values are too close together to be displayed neatly just separate them slightly
+    if ( Math.abs(midpoint - last_temp_y) > 10) {
     
 //adds the humidity onto the final value
-  g.selectAll("finalhumid")
+    g.selectAll("finalhumid")
           .data(obsData)
           .enter()
           .append("svg:text")
           .attr("x", chartWidth - margin )
           .attr("y", last_humidity_y)
-          .attr("font-family", "Arial,Helvetica,Verdana,sans-serif")
+          .attr("font-family", "sans-serif")
+          .attr("stroke-width", "0.5")
           .attr("font-size", "95%")
           .attr("dy", "0.25em")
           .attr("fill","blue")
           .text(obsData[lengthofdata - 1].rel_hum + "%");
           
 //adds the temperature onto the final value
-  g.selectAll("finaltemperature")
+    g.selectAll("finaltemperature")
           .data(obsData)
           .enter()
           .append("svg:text")
@@ -271,28 +298,24 @@ if ( midpoint - last_temp_y > 10)
           .attr("dy", "0.25em")
           .attr("fill","red")
           .text(obsData[lengthofdata - 1].air_temp + "°"); 
-}
-//This looks roundybout because the origin in d3 is at the top left
-// therefor the y values are actually calculated from the top, so a higher 
-// number actually means closer to the bottom
-  else 
-{   console.log("lesser")
-    if (last_temp_y < last_humidity_y)
-    {
-    topvalue = obsData[lengthofdata - 1].air_temp + "°",
-    bottomvalue = obsData[lengthofdata - 1].rel_hum + "%",
-    topcolour = "red",
-    bottomcolour = "blue"
     }
-  else
-    {
-    topvalue = obsData[lengthofdata - 1].rel_hum + "%",
-    bottomvalue = obsData[lengthofdata - 1].air_temp + "°",
-    topcolour = "blue",
-    bottomcolour = "red"    
-    };
+//This looks roundybout because the origin in d3 is at the top left
+// so a higher number means closer to the bottom
+    else {   
+        if (last_temp_y < last_humidity_y) {
+            topvalue = obsData[lengthofdata - 1].air_temp + "°",
+            bottomvalue = obsData[lengthofdata - 1].rel_hum + "%",
+            topcolour = "red",
+            bottomcolour = "blue"
+            }
+        else {
+             topvalue = obsData[lengthofdata - 1].rel_hum + "%",
+             bottomvalue = obsData[lengthofdata - 1].air_temp + "°",
+             topcolour = "blue",
+             bottomcolour = "red"    
+             };
     
-  g.selectAll("values_label")
+    g.selectAll("values_label")
             .data(obsData)
             .enter()
             .append("svg:text")
@@ -301,9 +324,9 @@ if ( midpoint - last_temp_y > 10)
             .attr("font-family", "sans-serif")
             .attr("font-size", "95%")
             .attr("dy", "0.25em")
-            .attr("fill",topcolour)
+            .attr("fill", topcolour)
             .text(topvalue);
-  g.selectAll("values_label")
+    g.selectAll("values_label")
             .data(obsData)
             .enter()
             .append("svg:text")
@@ -312,10 +335,12 @@ if ( midpoint - last_temp_y > 10)
             .attr("font-family", "sans-serif")
             .attr("font-size", "95%")
             .attr("dy", "0.25em")
-            .attr("fill",bottomcolour)
+            .attr("fill", bottomcolour)
             .text(bottomvalue);
-};
+    };
+    
 
-
-
+//try and add the forecast after the chart - if the URL works
+   $('<div id="import_forecast"></div>').load(forecast_url + ' .day.eve').insertBefore('.p-id');
+   $('<div id="import_forecast"></div>').load(forecast_url + ' .day.main').insertBefore('.p-id');
 });
